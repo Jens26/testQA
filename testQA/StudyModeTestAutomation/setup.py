fd4d9ee05604b2a3a3e8844ac6ff56e6138ded93
Jens <jsubhashish@gmail.com>
2018-02-26 14:31:15 +0530

    Added new pro

---
+++ testQA/StudyModeTestAutomation/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='StudyModeTestAutomation',
+    version='1.0',
+    packages=['utils', 'features', 'features.steps', 'features.utilities', 'features.utilities.core',
+              'features.utilities.actions', 'features.utilities.locators', 'features.utilities.pageactions'],
+    url='',
+    license='1.0',
+    author='studymode',
+    author_email='STM',
+    description=''
+)
